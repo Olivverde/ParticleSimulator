4b2e8e2ffbfa589f4dff66fc6daf83525ad303f8
Olivverde <dele19270@uvg.edu.gt>
2020-09-18 11:26:21 -0600

    Gathered information

--- Flasktry.py
+++ Flasktry.py
@@ -4,11 +4,14 @@ from flask import request
 import forms
 
 app = Flask(__name__, template_folder = "templates")
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
 	particle_form = forms.particularInfo(request.form)
 	if request.method == 'POST' and particle_form.validate():
 			print(particle_form.Vp.data)
+			print(particle_form.Ang.data)
+			print(particle_form.Field.data)
+			print(particle_form.particle.data)
 	return render_template('index.html', form = particle_form)
 if __name__ == '__main__':
     app.run(debug = True)	
